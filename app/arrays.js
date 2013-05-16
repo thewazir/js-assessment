0ba0a9e00fe51f6acdd9c93a13dd16d37e73c70a
Omer Wazir <omer.wazir@gmail.com>
2013-05-16 12:05:59 -0700

    Some array tests will pass

--- app/arrays.js
+++ app/arrays.js
@@ -3,47 +3,91 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+        return arr.indexOf(item);
     },
 
     sum : function(arr) {
-
+        var i = 0, 
+            sum = 0;
+        for ( ; i < arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
     },
 
     remove : function(arr, item) {
-
+        var i = 0;
+        for ( ; i < arr.length;i++){
+            if(arr[i] === item){
+                arr.splice(i,1);
+            }
+        }
+        return arr;
     },
     
-    removeWithoutCopy : function(arr, item) {
+    removeWithoutCopy : function(arr,item) {
+        var i = 0,
+            length = arr.length;
 
+        for ( ; i < length;i++) {
+            if(arr[i] === item){
+                arr.splice(i,1);
+                i-=1;
+                length-=1;
+            }
+        }
+        return arr;
     },
 
     append : function(arr, item) {
-
+        arr.push(item);
+        return arr;
     },
 
     truncate : function(arr) {
-
+         arr.pop();
+         return arr;
     },
 
     concat : function(arr1, arr2) {
-
+        return arr1.concat(arr2) ;
     },
 
     insert : function(arr, item, index) {
-
+        arr.splice(index,0,item);
+        return arr;
     },
 
     count : function(arr, item) {
-
+        var i =0,
+            length = arr.length,
+            count = 0;
+        for( ; i < length; i++){
+            if (arr[i] === item)
+            {
+                count += 1;
+            }
+        }
+        return count;
     },
 
     duplicates : function(arr) {
-
+        var i =0,
+            length = arr.length,
+            dupes = [];
+        for( ; i < length; i++)
+        {
+            if(this.count(arr,arr[i]) > 1)
+            {
+                dupes.push(arr[i]);
+                this.removeWithoutCopy(arr,arr[i]);
+            }
+        }
+        return dupes;
     },
 
     square : function(arr) {
-
+        console.log(arr);
     },
 
     findAllOccurrences : function(arr, target) {
